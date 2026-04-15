7463962036aebec2da7bbf829ec5708c65b58eb0
cy <1181160309@qq.com>
2026-04-15 09:33:51 +0800

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\cy\downloads\源代码\书本配套代码\第10章\hengdaproject\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
